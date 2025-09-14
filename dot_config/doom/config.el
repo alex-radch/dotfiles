87b062105a93117b0a01e57b41af59a830da5adc
Alex <radch.alex97@gmail.com>
2025-09-15 00:31:24 +0300

    added kanagawa to doom emacs

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -22,7 +22,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(load-theme 'kanagawa-wave t)
 
 (setq doom-font (font-spec :family "CaskaydiaMono Nerd Font" :size 22))
      ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
